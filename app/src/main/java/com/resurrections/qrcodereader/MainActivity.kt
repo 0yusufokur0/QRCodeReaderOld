4927bab138c03373e15950e8994a60d752cc0f79
Yusuf Okur <yusuf.okur@veripark.com>
2022-01-26 14:22:47 +0300

    Project was created.

---
+++ app/src/main/java/com/resurrections/qrcodereader/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.resurrections.qrcodereader
+
+import android.Manifest.permission
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.SparseArray
+import android.view.SurfaceHolder
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import com.resurrections.qrcodereader.databinding.ActivityMainBinding
+import com.resurrections.qrcodereader.databinding.DetectionResultLayoutBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val taskHandler = Handler(Looper.getMainLooper())
+    private lateinit var detector: BarcodeDetector
+    private lateinit var cameraSource: CameraSource
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var builder: AlertDialog.Builder
+    private lateinit var dialogBinding : DetectionResultLayoutBinding
+    private lateinit var alertDialog: AlertDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpDetector()
+        setUpCamera()
+        setUpSurface()
+        setUpAlertDialog()
+    }
+
+    private fun setUpAlertDialog() {
+        builder = AlertDialog.Builder(this)
+        dialogBinding = DetectionResultLayoutBinding.inflate(layoutInflater)
+        builder.setView(dialogBinding.root)
+        builder.setCancelable(false)
+        builder.setPositiveButton("continue") { dialog, which ->
+            startCamera()
+        }
+
+        builder.setNegativeButton("save and continue") { dialog, which ->
+            // save room db
+        }
+
+        builder.setNeutralButton("cancel") { dialog, which ->
+            finish()
+        }
+
+        alertDialog = builder.create()
+
+    }
+
+    private fun setUpDetector(){
+        detector = BarcodeDetector
+            .Builder(this)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
+
+        detector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {}
+            override fun receiveDetections(p0: Detector.Detections<Barcode>?) = detectionResult(p0?.detectedItems)
+        })
+    }
+
+    private fun setUpCamera(){
+        cameraSource = CameraSource
+            .Builder(this, detector)
+            .setRequestedPreviewSize(1024, 768)
+            .setRequestedFps(30f)
+            .setAutoFocusEnabled(true)
+            .build()
+    }
+
+    private fun setUpSurface(){
+        binding.barcodeSurfaceView.holder.addCallback(object : SurfaceHolder.Callback2 {
+            override fun surfaceCreated(p0: SurfaceHolder) = checkPermission()
+            override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) = println()
+            override fun surfaceDestroyed(p0: SurfaceHolder) = cameraSource.stop()
+            override fun surfaceRedrawNeeded(p0: SurfaceHolder) = println()
+        })
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startCamera(): CameraSource = cameraSource.start(binding.barcodeSurfaceView.holder)
+
+    private fun stopCamera() = cameraSource.stop()
+
+    private fun detectionResult(barcodes: SparseArray<Barcode>?) {
+        taskHandler.post {
+            try {
+
+                dialogBinding.resultTextView.text = barcodes?.valueAt(0)?.displayValue
+                alertDialog.show()
+                stopCamera()
+                taskHandler.removeCallbacksAndMessages(null)
+            } catch (e: Exception) {
+                startCamera()
+            }
+        }
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+            startCamera()
+        else ActivityCompat.requestPermissions(this, arrayOf(permission.CAMERA), 123)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                startCamera()
+            else Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        detector.release()
+        stopCamera()
+        cameraSource.release()
+    }
+}
